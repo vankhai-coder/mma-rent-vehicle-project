f8e2bf213072ebea1fbe70924459c716d474512b
thaibao02 <thaibaovu0212@gmail.com>
2025-02-19 15:47:39 +0700

    update in SearchScreen.tsx
    change method search , list vehicles will display follow input on text search

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
       {/* <ManageRental /> */}
 
       {/* Thai Bao :  */}
-      {/* <SearchScreen /> */}
+      <SearchScreen />
       {/* <SignUpScreen /> */}
       {/* <VehicleDetailScreen /> */}
 
